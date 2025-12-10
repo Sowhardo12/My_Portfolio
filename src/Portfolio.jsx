9678d8292e1940b56524610aff3f20acd17c638c
Sowhardo Bin Atik <105670032+Sowhardo12@users.noreply.github.com>
2025-12-10 23:02:43 +0600

    changed some color and minor changes

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -9,14 +9,14 @@ function Portfolio() {
       <div className='max-w-4xl mx-auto px-6'>
         <header className='flex items-center justify-between py-6'>
           <a href='#home' className='inline-flex items-center gap-3'>
-            <span className='text-lg font-medium'>Sowhardo Bin Atik</span>
+            <span className='text-lg font-medium'> &lt; Sowhardo Bin Atik /&gt;</span>
           </a>
           <nav className='hidden md:flex items-center gap-6 text-sm'>
-            <a href='#about' className='hover: text-indigo-600'>About</a>
+            <a href='#about' className='hover:text-indigo-600'>About</a>
             <a href="#projects" className="hover:text-indigo-600">Projects</a>
             <a href="#skills" className="hover:text-indigo-600">Skills</a>
             <a href="#contact" className="hover:text-indigo-600">Contact</a>
-            <a href="#resume" className="ml-4 px-3 py-1 border rounded-md text-sm hover:bg-slate-50">Resume</a>
+            <a href="#resume" className="ml-4 px-3 py-1 border rounded-md text-sm hover:bg-red-600">Resume</a>
           </nav>
           <button 
           className='md:hidden p-2 rounded-md focus:outline-none'
@@ -43,6 +43,7 @@ function Portfolio() {
           </nav>
         </div>
 
+        {/* Main section here */}  
         <main id='home' className='py-12'>
           <div className='grid gap-8 md:grid-cols-2 md:items-center'>
             <div>
@@ -53,8 +54,8 @@ function Portfolio() {
                 I build minimal, reliable web apps using React and modern tools. Currently putting my focus on learning MERN stack and Algorithmic Problem solving on LeetCode. I love to solve problems, learn technological concepts and build.
               </p>
               <div className='mt-6 flex gap-3'>
-                <a href="#projects" className='inline-block px-5 py-2 rounded-md bg-blue-600 text-white font-medium shadow-sm hover:opacity-60'>View Projects</a>
-                <a href="#contacts" className='inline-block px-5 py-2 rounded-md border text-sm hover:bg-slate-200'>Contact Me</a>
+                <a href="#projects" className='inline-block px-5 py-2 rounded-md bg-orange-600 text-white font-medium shadow-sm hover:opacity-60'>View Projects</a>
+                <a href="#contacts" className='inline-block px-5 py-2 rounded-md border text-sm hover:bg-red-400'>Contact Me</a>
               </div> 
             </div>
             <div className="hidden md:flex items-center justify-center">
